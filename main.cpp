ecf2ecd5a9d47e84b9fc2f1646c787f562b83fbe
Daniel Pfeifer <daniel@pfeifer-mail.de>
2026-08-15 16:26:14 +0200

    use-observers: use annotations

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <clang-tidy/ClangTidyModule.h>
 
-#include "observers.h"
 #include "outpoint-assignment.h"
 #include "tx-deref.h"
+#include "use-observers.h"
 
 class BitcoinModule final : public clang::tidy::ClangTidyModule
 {
@@ -10,15 +10,9 @@ public:
   void addCheckFactories(
     clang::tidy::ClangTidyCheckFactories& Factories) override
   {
-    Factories.registerCheck<BlockObservers>("bitcoin-block-observers");
-    Factories.registerCheck<CoinObservers>("bitcoin-coin-observers");
     Factories.registerCheck<OutpointAssignment>("bitcoin-outpoint-assignment");
-    Factories.registerCheck<OutpointObservers>("bitcoin-outpoint-observers");
     Factories.registerCheck<TxDeref>("bitcoin-tx-deref");
-    Factories.registerCheck<TxinObservers>("bitcoin-txin-observers");
-    Factories.registerCheck<TxObservers>("bitcoin-tx-observers");
-    Factories.registerCheck<TxMutObservers>("bitcoin-txmut-observers");
-    Factories.registerCheck<TxoutObservers>("bitcoin-txout-observers");
+    Factories.registerCheck<UseObservers>("bitcoin-use-observers");
   }
 };
 
